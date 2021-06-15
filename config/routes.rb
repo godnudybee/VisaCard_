4f035bbeb2c0c123a9125a4cf44e7c7c8de5c2f7
godnudybee <boninnutepe@hotmail.com>
2021-06-15 15:21:29 +0000

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get 'utilisateur', to: 'users#utilisateurs'
-  post 'utilisateur', to: 'users#utilisateurs'
+  get 'utilisateur', to: 'fx_kudi#utilisateurs'
+  post 'utilisateur', to: 'fx_kudi#utilisateurs'
   get 'u', to: "fx_kudi#u"
   post 'u', to: "fx_kudi#u"
 
