0ea19454e7d5cda1b4225c00681fe72864688401
godnudybee <boninnutepe@hotmail.com>
2021-06-15 16:07:59 +0000

    Update fx_kudi_controller.rb

--- app/controllers/fx_kudi_controller.rb
+++ app/controllers/fx_kudi_controller.rb
@@ -1,8 +1,11 @@
 class FxKudiController < ApplicationController
     def utilisateurs    
         #@data = params
-        
-        render :text => "1"
+
+        respond_to do |format|
+            msg = { :status => "1" }
+            format.json  { render :json => msg } # don't do msg.to_json
+        end
     end
     
     def u
