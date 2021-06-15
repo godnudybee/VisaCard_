39bfa0f4b10b7ea78f49669974f08c1ce4150853
godnudybee <boninnutepe@hotmail.com>
2021-06-15 15:04:31 +0000

    fxkudi

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Visacard
   class Application < Rails::Application
+    config.api_only = true
+    
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
@@ -18,5 +20,6 @@ module Visacard
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+  
   end
 end
